d8ccfa842f4f5849905242549e126999cb6d323c
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-11-19 09:49:37 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("white");
+
+  // function changeColor(color) {
+  //   setColor(color);
+  // }
+
+  return (
+    <div className="w-full h-screen duration-1000" style={{ background: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-2 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("linear-gradient(to right, red, green)")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundImage: "linear-gradient(to right, red, green)" }}
+          >
+            Red & Green Gradient
+          </button>
+          <button
+            onClick={() => setColor("linear-gradient(to right, red, blue)")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundImage: "linear-gradient(to right, red, blue)" }}
+          >
+            Red & Blue Gradient
+          </button>
+          <button
+            onClick={() => setColor("linear-gradient(to right, green, blue)")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundImage: "linear-gradient(to right, green, blue)" }}
+          >
+            Green & Blue Gradient
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
